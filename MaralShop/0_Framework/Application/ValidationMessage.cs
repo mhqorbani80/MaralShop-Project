4214dd37927fc4f2c0063616769fcc74d6500a6c
Mohammad Ghorbani <mohamadghorbanii1380@gmail.com>
2022-04-13 13:44:03 +0430

    ValidationClient ProductCategory

---
+++ MaralShop/0_Framework/Application/ValidationMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace _0_Framework.Application
+{
+    public class ValidationMessage
+    {
+        public const string IsRequired = "این مقدار نمی تواند خالی باشد. لطفا کامل کنید.";
+    }
+}
--- MaralShop/ShopManagement.Application.Contracts/ProductCategory/CreateProductCategory.cs
+++ MaralShop/ShopManagement.Application.Contracts/ProductCategory/CreateProductCategory.cs
@@ -1,14 +1,21 @@
-﻿namespace ShopManagement.Application.Contracts.ProductCategory
+﻿using _0_Framework.Application;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShopManagement.Application.Contracts.ProductCategory
 {
     public class CreateProductCategory
     {
-        public string Name { get;  set; }
-        public string Description { get;  set; }
-        public string Picture { get;  set; }
-        public string PictureAlt { get;  set; }
-        public string PictureTitle { get;  set; }
-        public string Keywords { get;  set; }
-        public string MetaDescription { get;  set; }
-        public string Slug { get;  set; }
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
+        public string? Name { get;  set; }
+        public string? Description { get;  set; }
+        public string? Picture { get;  set; }
+        public string? PictureAlt { get;  set; }
+        public string? PictureTitle { get;  set; }
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
+        public string? Keywords { get;  set; }
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
+        public string? MetaDescription { get;  set; }
+        [Required(ErrorMessage = ValidationMessage.IsRequired)]
+        public string? Slug { get;  set; }
     }
 }
